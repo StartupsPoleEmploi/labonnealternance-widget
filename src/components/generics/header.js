b01f231e3f0e556cfaa26a86a5dfbe32ef6905ba
Alexandre CANTIN <cantin.alexandre@gmail.com>
2019-04-14 11:36:10 +0200

    Use new monochrome logo

--- src/components/generics/header.js
+++ src/components/generics/header.js
@@ -5,11 +5,11 @@ export default function Header({ dispatchFn }) {
     return (
         <header>
             <Link step={WIDGET_STEPS.HOME} title="Retour à l'accueil du widget" dispatchFn={dispatchFn}>
-                <img src="http://labonnealternance.pole-emploi.fr/static/img/logo/logo-noir-lba.svg" alt="La Bonne Alternance" />
+                <img src="http://labonnealternance.pole-emploi.fr/static/img/logo/logo-noir-only-lba.svg" alt="Logo - La Bonne Alternance - Pôle Emploi" />
             </Link>
 
             <a href="https://www.pole-emploi.fr/" rel="noreferrer noopener" target="_blank" title="Aller sur pole-emploi.fr (ouverture dans un nouvel onglet)">
-                <img src="http://labonnealternance.pole-emploi.fr/static/img/logo/pole-emploi-couleur.svg" alt="Pôle Emploi" />
+                <img src="http://labonnealternance.pole-emploi.fr/static/img/logo/pole-emploi-couleur.svg" alt="Logo Pôle Emploi" />
             </a>
         </header>
     )
